1df262df3d48b72b2e69f52df25b74044803cd23
Miky-131 <cryptogame131@gmail.com>
2023-11-01 05:36:48 +1000

    update

--- js/bundle.js
+++ js/bundle.js
@@ -7099,10 +7099,10 @@
       const formattedSold = window.ethers.utils.formatUnits(sold, 18);
       const formattedTotalSupply = window.ethers.utils.formatUnits(totalsupply, 18);
       console.log("balance", formattedSold, formattedTotalSupply)
-      // document.getElementById("percent").innerHTML = (
-      //   (Number(formattedSold) / Number(formattedTotalSupply)) *
-      //   100
-      // ).toFixed(2);
+      document.getElementById("currentprogress").innerHTML = (
+        (Number(formattedSold) / Number(formattedTotalSupply)) *
+        100
+      ).toFixed(2);
     }
     getTokenBalance();
 
