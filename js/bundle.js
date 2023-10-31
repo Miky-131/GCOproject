96a927a0ab35006f53d2d69ee68b11884cba797e
Miky-131 <cryptogame131@gmail.com>
2023-11-01 03:30:06 +1000

    update

--- js/bundle.js
+++ js/bundle.js
@@ -7092,7 +7092,7 @@
     var provider = new window.ethers.providers.Web3Provider(window.ethereum);
     var contract = new window.ethers.Contract(CONTRACT_ADDRESS, window.GCOToken.abi, provider.getSigner(0));
     // const totalSupply = await contract.totalSupply();
-    // const balance = await contract.balanceOf("0xc1361d79F1376807d123bE955B2d4744Df1cD838");
+    const balance = await contract.balanceOf("0xc1361d79F1376807d123bE955B2d4744Df1cD838");
     // const sold = totalSupply.sub(balance);
     // const formattedSold = window.ethers.utils.formatUnits(sold, 18);
     // const formattedTotalSupply = window.ethers.utils.formatUnits(totalSupply, 18);
